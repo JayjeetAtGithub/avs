2747a1d5b6a8fd8a28e037b8570bbe37f9dba499
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-11-14 07:24:26 +0000

    Complete ip

--- example.cpp
+++ example.cpp
@@ -7,7 +7,7 @@
 
 
 void print_matrix(std::vector<std::vector<float>> &mat) {
-    for (int i = 0; i < mat.size(); i++) {
+    for (int i = 0; i < std::min(mat.size(), 10); i++) {
         for (int j = 0; j < mat[0].size(); j++) {
             std::cout << mat[i][j] << " ";
         }
