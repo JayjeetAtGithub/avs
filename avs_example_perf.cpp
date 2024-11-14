13cda56bb37e8625574d0f2d7b4b16ba9f0cfbaa
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-11-14 21:04:05 +0000

    Add ip vanilla perf

--- avs_example_perf.cpp
+++ avs_example_perf.cpp
@@ -76,6 +76,12 @@ int main(int argc, char **argv) {
     std::cout << "Duration (L2 vanilla): " << dur_ms << std::endl;
 
     s = std::chrono::high_resolution_clock::now();
+    knn_index->search_ip_vanilla_perf(queries, top_k);
+    e = std::chrono::high_resolution_clock::now();
+    dur_ms = std::chrono::duration_cast<std::chrono::milliseconds>(e-s).count();
+    std::cout << "Duration (IP vanilla): " << dur_ms << std::endl;
+
+    s = std::chrono::high_resolution_clock::now();
     knn_index->search_ip_perf(queries, top_k);
     e = std::chrono::high_resolution_clock::now();
     dur_ms = std::chrono::duration_cast<std::chrono::milliseconds>(e-s).count();
