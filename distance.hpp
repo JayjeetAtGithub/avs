816cc206cde9e769eedf2a24e27ebeedb20a3a10
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-11-15 07:36:32 +0000

    format distance.hpp

--- distance.hpp
+++ distance.hpp
@@ -2,8 +2,8 @@
 
 #include <unordered_map>
 
-#include "utils.hpp"
 #include "oneapi/dnnl/dnnl.hpp"
+#include "utils.hpp"
 
 #if defined(__GNUC__)
 #define PORTABLE_ALIGN32 __attribute__((aligned(32)))
@@ -21,236 +21,228 @@ using tag = dnnl::memory::format_tag;
 using dt = dnnl::memory::data_type;
 
 inline static bool is_amxbf16_supported() {
-    unsigned int eax, ebx, ecx, edx;
-    __asm__ __volatile__("cpuid"
-                         : "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx)
-                         : "a"(7), "c"(0));
-    return edx & (1 << 22);
+  unsigned int eax, ebx, ecx, edx;
+  __asm__ __volatile__("cpuid"
+                       : "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx)
+                       : "a"(7), "c"(0));
+  return edx & (1 << 22);
 }
 
-void avx512_subtract(
-    float const *a, float const *b, float *c) {
-    __m512 diff, v1, v2;
-    v1 = _mm512_loadu_ps(a);
-    v2 = _mm512_loadu_ps(b);
-    diff = _mm512_sub_ps(v1, v2);
-    _mm512_store_ps(c, diff);
+void avx512_subtract(float const *a, float const *b, float *c) {
+  __m512 diff, v1, v2;
+  v1 = _mm512_loadu_ps(a);
+  v2 = _mm512_loadu_ps(b);
+  diff = _mm512_sub_ps(v1, v2);
+  _mm512_store_ps(c, diff);
 }
 
-avs::matf32_t avx512_subtract_batch(
-    avs::vecf32_t const &query, avs::matf32_t const &data) {
-    int32_t const N = data.size();
-    int32_t const dim = data[0].size();
-    avs::matf32_t result(N, avs::vecf32_t(dim, 0.0f));
-    for (int i = 0; i < N; i++) {
-        float PORTABLE_ALIGN64 tmp[dim];
-        avx512_subtract(query.data(), data[i].data(), tmp);
-        for (int k = 0; k < dim; k++) {
-            result[i][k] = tmp[k];
-        }
+avs::matf32_t avx512_subtract_batch(avs::vecf32_t const &query,
+                                    avs::matf32_t const &data) {
+  int32_t const N = data.size();
+  int32_t const dim = data[0].size();
+  avs::matf32_t result(N, avs::vecf32_t(dim, 0.0f));
+  for (int i = 0; i < N; i++) {
+    float PORTABLE_ALIGN64 tmp[dim];
+    avx512_subtract(query.data(), data[i].data(), tmp);
+    for (int k = 0; k < dim; k++) {
+      result[i][k] = tmp[k];
     }
-    return result;
+  }
+  return result;
 }
 
-avs::vecf32_t amx_matmul(
-    const int64_t &r, const int64_t &c,
-    avs::vecf32_t const &m, avs::vecf32_t const &mt,
-    dnnl::engine &engine, dnnl::stream &stream) {
-    avs::vecf32_t dst(r * r, 0.0f);
+avs::vecf32_t amx_matmul(const int64_t &r, const int64_t &c,
+                         avs::vecf32_t const &m, avs::vecf32_t const &mt,
+                         dnnl::engine &engine, dnnl::stream &stream) {
+  avs::vecf32_t dst(r * r, 0.0f);
 
-    dnnl::memory::dims a_dims = {r, c};
-    dnnl::memory::dims b_dims = {c, r};
-    dnnl::memory::dims c_dims = {r, r};
+  dnnl::memory::dims a_dims = {r, c};
+  dnnl::memory::dims b_dims = {c, r};
+  dnnl::memory::dims c_dims = {r, r};
 
-    // Declare fp32 input memory descriptors
-    auto a_in_md = dnnl::memory::desc(a_dims, dt::f32, tag::ab);
-    auto b_in_md = dnnl::memory::desc(b_dims, dt::f32, tag::ab);
-    auto c_out_md = dnnl::memory::desc(c_dims, dt::f32, tag::ab);
-    auto a_in_mem = dnnl::memory(a_in_md, engine);
-    auto b_in_mem = dnnl::memory(b_in_md, engine);
-    write_to_dnnl_memory(m.data(), a_in_mem);
-    write_to_dnnl_memory(mt.data(), b_in_mem);
+  // Declare fp32 input memory descriptors
+  auto a_in_md = dnnl::memory::desc(a_dims, dt::f32, tag::ab);
+  auto b_in_md = dnnl::memory::desc(b_dims, dt::f32, tag::ab);
+  auto c_out_md = dnnl::memory::desc(c_dims, dt::f32, tag::ab);
+  auto a_in_mem = dnnl::memory(a_in_md, engine);
+  auto b_in_mem = dnnl::memory(b_in_md, engine);
+  write_to_dnnl_memory(m.data(), a_in_mem);
+  write_to_dnnl_memory(mt.data(), b_in_mem);
 
-    auto a_md = dnnl::memory::desc(a_dims, dt::bf16, tag::any);
-    auto b_md = dnnl::memory::desc(b_dims, dt::bf16, tag::any);
-    auto pd = dnnl::matmul::primitive_desc(engine, a_md, b_md, c_out_md);
-    
-    auto a_mem = dnnl::memory(pd.src_desc(), engine);
-    dnnl::reorder(a_in_mem, a_mem).execute(stream, a_in_mem, a_mem);
-    auto b_mem = dnnl::memory(pd.weights_desc(), engine);
-    dnnl::reorder(b_in_mem, b_mem).execute(stream, b_in_mem, b_mem);
-    auto c_mem = dnnl::memory(pd.dst_desc(), engine);
+  auto a_md = dnnl::memory::desc(a_dims, dt::bf16, tag::any);
+  auto b_md = dnnl::memory::desc(b_dims, dt::bf16, tag::any);
+  auto pd = dnnl::matmul::primitive_desc(engine, a_md, b_md, c_out_md);
 
-    auto prim = dnnl::matmul(pd);
-    std::unordered_map<int, dnnl::memory> args;
-    args.insert({DNNL_ARG_SRC, a_mem});
-    args.insert({DNNL_ARG_WEIGHTS, b_mem});
-    args.insert({DNNL_ARG_DST, c_mem});
-    prim.execute(stream, args);
-    stream.wait();
+  auto a_mem = dnnl::memory(pd.src_desc(), engine);
+  dnnl::reorder(a_in_mem, a_mem).execute(stream, a_in_mem, a_mem);
+  auto b_mem = dnnl::memory(pd.weights_desc(), engine);
+  dnnl::reorder(b_in_mem, b_mem).execute(stream, b_in_mem, b_mem);
+  auto c_mem = dnnl::memory(pd.dst_desc(), engine);
 
-    read_from_dnnl_memory(dst.data(), c_mem);
-    avs::vecf32_t res(r, 0.0f);
-    for (int i = 0; i < r; i++) {
-        res[i] = dst[i * r + i];
-    }
-    return res;
+  auto prim = dnnl::matmul(pd);
+  std::unordered_map<int, dnnl::memory> args;
+  args.insert({DNNL_ARG_SRC, a_mem});
+  args.insert({DNNL_ARG_WEIGHTS, b_mem});
+  args.insert({DNNL_ARG_DST, c_mem});
+  prim.execute(stream, args);
+  stream.wait();
+
+  read_from_dnnl_memory(dst.data(), c_mem);
+  avs::vecf32_t res(r, 0.0f);
+  for (int i = 0; i < r; i++) {
+    res[i] = dst[i * r + i];
+  }
+  return res;
 }
 
-static matf32_t amx_inner_product(
-    int32_t const &n, int32_t const &oc, int32_t const &ic,
-    avs::vecf32_t const &s, avs::vecf32_t const &w,
-    dnnl::engine &engine, dnnl::stream &stream) {
+static matf32_t amx_inner_product(int32_t const &n, int32_t const &oc,
+                                  int32_t const &ic, avs::vecf32_t const &s,
+                                  avs::vecf32_t const &w, dnnl::engine &engine,
+                                  dnnl::stream &stream) {
 
-    dnnl::memory::dims s_dims = {n, ic};
-    dnnl::memory::dims w_dims = {oc, ic};
-    dnnl::memory::dims dst_dims = {n, oc};
+  dnnl::memory::dims s_dims = {n, ic};
+  dnnl::memory::dims w_dims = {oc, ic};
+  dnnl::memory::dims dst_dims = {n, oc};
 
-    auto s_in_md = dnnl::memory::desc(s_dims, dt::f32, tag::ab);
-    auto w_in_md = dnnl::memory::desc(w_dims, dt::f32, tag::ab);
-    auto dst_out_md = dnnl::memory::desc(dst_dims, dt::f32, tag::ab);
-    auto s_in_mem = dnnl::memory(s_in_md, engine);
-    auto w_in_mem = dnnl::memory(w_in_md, engine);
-    write_to_dnnl_memory(s.data(), s_in_mem);
-    write_to_dnnl_memory(w.data(), w_in_mem);
+  auto s_in_md = dnnl::memory::desc(s_dims, dt::f32, tag::ab);
+  auto w_in_md = dnnl::memory::desc(w_dims, dt::f32, tag::ab);
+  auto dst_out_md = dnnl::memory::desc(dst_dims, dt::f32, tag::ab);
+  auto s_in_mem = dnnl::memory(s_in_md, engine);
+  auto w_in_mem = dnnl::memory(w_in_md, engine);
+  write_to_dnnl_memory(s.data(), s_in_mem);
+  write_to_dnnl_memory(w.data(), w_in_mem);
 
-    auto s_md = dnnl::memory::desc(s_dims, dt::bf16, tag::any);
-    auto w_md = dnnl::memory::desc(w_dims, dt::bf16, tag::any);
+  auto s_md = dnnl::memory::desc(s_dims, dt::bf16, tag::any);
+  auto w_md = dnnl::memory::desc(w_dims, dt::bf16, tag::any);
 
-    auto pd = dnnl::inner_product_forward::primitive_desc(
-        engine,
-        dnnl::prop_kind::forward_training, 
-        s_md, 
-        w_md,
-        dst_out_md);
+  auto pd = dnnl::inner_product_forward::primitive_desc(
+      engine, dnnl::prop_kind::forward_training, s_md, w_md, dst_out_md);
 
-    auto s_mem = dnnl::memory(pd.src_desc(), engine);
-    dnnl::reorder(s_in_mem, s_mem).execute(stream, s_in_mem, s_mem);
-    auto w_mem = dnnl::memory(pd.weights_desc(), engine);
-    dnnl::reorder(w_in_mem, w_mem).execute(stream, w_in_mem, w_mem);
-    auto dst_mem = dnnl::memory(pd.dst_desc(), engine);
+  auto s_mem = dnnl::memory(pd.src_desc(), engine);
+  dnnl::reorder(s_in_mem, s_mem).execute(stream, s_in_mem, s_mem);
+  auto w_mem = dnnl::memory(pd.weights_desc(), engine);
+  dnnl::reorder(w_in_mem, w_mem).execute(stream, w_in_mem, w_mem);
+  auto dst_mem = dnnl::memory(pd.dst_desc(), engine);
 
-    auto prim = dnnl::inner_product_forward(pd);
-    std::unordered_map<int, dnnl::memory> args;
-    args.insert({DNNL_ARG_SRC, s_mem});
-    args.insert({DNNL_ARG_WEIGHTS, w_mem});
-    args.insert({DNNL_ARG_DST, dst_mem});
-    prim.execute(stream, args);
-    stream.wait();
+  auto prim = dnnl::inner_product_forward(pd);
+  std::unordered_map<int, dnnl::memory> args;
+  args.insert({DNNL_ARG_SRC, s_mem});
+  args.insert({DNNL_ARG_WEIGHTS, w_mem});
+  args.insert({DNNL_ARG_DST, dst_mem});
+  prim.execute(stream, args);
+  stream.wait();
 
-    avs::vecf32_t dst(n * oc, 0.0f);
-    read_from_dnnl_memory(dst.data(), dst_mem);
-    avs::matf32_t res(n, avs::vecf32_t(oc, 0.0f));
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < oc; j++) {
-            res[i][j] = dst[i * oc + j];
-        }
+  avs::vecf32_t dst(n * oc, 0.0f);
+  read_from_dnnl_memory(dst.data(), dst_mem);
+  avs::matf32_t res(n, avs::vecf32_t(oc, 0.0f));
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < oc; j++) {
+      res[i][j] = dst[i * oc + j];
     }
-    return res;
+  }
+  return res;
 }
 
-static avs::matf32_t ip_distance_amx(
-    avs::matf32_t const &queries, avs::matf32_t const &batch, 
-    dnnl::engine &engine, dnnl::stream &stream) {
-    const int32_t n = queries.size();
-    const int32_t oc = batch.size();
-    const int32_t ic = queries[0].size();
-    avs::vecf32_t queries_1d(n * ic);
-    avs::vecf32_t batch_1d(oc * ic);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < ic; j++) {
-            queries_1d[i * ic + j] = queries[i][j];
-        }
+static avs::matf32_t ip_distance_amx(avs::matf32_t const &queries,
+                                     avs::matf32_t const &batch,
+                                     dnnl::engine &engine,
+                                     dnnl::stream &stream) {
+  const int32_t n = queries.size();
+  const int32_t oc = batch.size();
+  const int32_t ic = queries[0].size();
+  avs::vecf32_t queries_1d(n * ic);
+  avs::vecf32_t batch_1d(oc * ic);
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < ic; j++) {
+      queries_1d[i * ic + j] = queries[i][j];
     }
-    for (int i = 0; i < oc; i++) {
-        for (int j = 0; j < ic; j++) {
-            batch_1d[i * ic + j] = batch[i][j];
-        }
+  }
+  for (int i = 0; i < oc; i++) {
+    for (int j = 0; j < ic; j++) {
+      batch_1d[i * ic + j] = batch[i][j];
     }
-    return amx_inner_product(
-        n, oc, ic,
-        queries_1d, batch_1d, 
-        engine, stream
-    );
+  }
+  return amx_inner_product(n, oc, ic, queries_1d, batch_1d, engine, stream);
 }
 
-static avs::vecf32_t l2_distance_amx(
-    avs::vecf32_t const &query, avs::matf32_t const &batch, 
-    dnnl::engine &engine, dnnl::stream &stream) {
-    const int64_t batch_size = batch.size();
-    const int64_t dim = batch[0].size();
-    avs::matf32_t dis_2d = avx512_subtract_batch(query, batch);
-    avs::matf32_t dis_2d_t(
-        dis_2d[0].size(), avs::vecf32_t(dis_2d.size(), 0.0f));
-    for (int i = 0; i < dis_2d_t.size(); i++) {
-        for (int j = 0; j < dis_2d_t[0].size(); j++) {
-            dis_2d_t[i][j] = dis_2d[j][i];
-        }
+static avs::vecf32_t l2_distance_amx(avs::vecf32_t const &query,
+                                     avs::matf32_t const &batch,
+                                     dnnl::engine &engine,
+                                     dnnl::stream &stream) {
+  const int64_t batch_size = batch.size();
+  const int64_t dim = batch[0].size();
+  avs::matf32_t dis_2d = avx512_subtract_batch(query, batch);
+  avs::matf32_t dis_2d_t(dis_2d[0].size(), avs::vecf32_t(dis_2d.size(), 0.0f));
+  for (int i = 0; i < dis_2d_t.size(); i++) {
+    for (int j = 0; j < dis_2d_t[0].size(); j++) {
+      dis_2d_t[i][j] = dis_2d[j][i];
     }
-    avs::vecf32_t dis_1d(batch_size * dim);
-    for (int i = 0; i < batch_size; i++) {
-        for (int j = 0; j < dim; j++) {
-            dis_1d[i * dim + j] = dis_2d[i][j];
-        }
+  }
+  avs::vecf32_t dis_1d(batch_size * dim);
+  for (int i = 0; i < batch_size; i++) {
+    for (int j = 0; j < dim; j++) {
+      dis_1d[i * dim + j] = dis_2d[i][j];
     }
-    avs::vecf32_t dis_1d_t(batch_size * dim);
-    for (int i = 0; i < dim; i++) {
-        for (int j = 0; j < batch_size; j++) {
-            dis_1d_t[i * batch_size + j] = dis_2d_t[i][j];
-        }
+  }
+  avs::vecf32_t dis_1d_t(batch_size * dim);
+  for (int i = 0; i < dim; i++) {
+    for (int j = 0; j < batch_size; j++) {
+      dis_1d_t[i * batch_size + j] = dis_2d_t[i][j];
     }
-    return amx_matmul(batch_size, dim, dis_1d, dis_1d_t, engine, stream);
+  }
+  return amx_matmul(batch_size, dim, dis_1d, dis_1d_t, engine, stream);
 }
 
-static float L2Sqr(
-    void const *vec1, void const *vec2, int32_t const &dim) {
-    float *v1 = (float *) vec1;
-    float *v2 = (float *) vec2;
+static float L2Sqr(void const *vec1, void const *vec2, int32_t const &dim) {
+  float *v1 = (float *)vec1;
+  float *v2 = (float *)vec2;
 
-    float res = 0;
-    for (size_t i = 0; i < dim; i++) {
-        float t = *v1 - *v2;
-        v1++;
-        v2++;
-        res += t * t;
-    }
-    return (res);
+  float res = 0;
+  for (size_t i = 0; i < dim; i++) {
+    float t = *v1 - *v2;
+    v1++;
+    v2++;
+    res += t * t;
+  }
+  return (res);
 }
 
-static float InnerProduct(
-    void const *vec1, void const *vec2, int32_t const &dim) {
-    float *v1 = (float *) vec1;
-    float *v2 = (float *) vec2;
+static float InnerProduct(void const *vec1, void const *vec2,
+                          int32_t const &dim) {
+  float *v1 = (float *)vec1;
+  float *v2 = (float *)vec2;
 
-    float res = 0;
-    for (size_t i = 0; i < dim; i++) {
-        res += ((float *) v1)[i] * ((float *) v2)[i];
-    }
-    return res;
+  float res = 0;
+  for (size_t i = 0; i < dim; i++) {
+    res += ((float *)v1)[i] * ((float *)v2)[i];
+  }
+  return res;
 }
 
-static avs::vecf32_t l2_distance_vanilla(
-avs::vecf32_t const &query, avs::matf32_t const &batch, 
-  dnnl::engine &engine, dnnl::stream &stream) {
-    int64_t const dim = batch[0].size();
-    avs::vecf32_t res(batch.size());
-    for (int i = 0; i < batch.size(); i++) {
-        auto d = L2Sqr(query.data(), batch[i].data(), dim);
-        res[i] = d;
-    }
-    return res;
+static avs::vecf32_t l2_distance_vanilla(avs::vecf32_t const &query,
+                                         avs::matf32_t const &batch,
+                                         dnnl::engine &engine,
+                                         dnnl::stream &stream) {
+  int64_t const dim = batch[0].size();
+  avs::vecf32_t res(batch.size());
+  for (int i = 0; i < batch.size(); i++) {
+    auto d = L2Sqr(query.data(), batch[i].data(), dim);
+    res[i] = d;
+  }
+  return res;
 }
 
-static avs::vecf32_t ip_distance_vanilla(
-    avs::vecf32_t const &query, avs::matf32_t const &batch, 
-    dnnl::engine &engine, dnnl::stream &stream) {
-    int64_t const dim = batch[0].size();
-    avs::vecf32_t res(batch.size());
-    for (int i = 0; i < batch.size(); i++) {
-        auto d = InnerProduct(query.data(), batch[i].data(), dim);
-        res[i] = d;
-    }
-    return res;
+static avs::vecf32_t ip_distance_vanilla(avs::vecf32_t const &query,
+                                         avs::matf32_t const &batch,
+                                         dnnl::engine &engine,
+                                         dnnl::stream &stream) {
+  int64_t const dim = batch[0].size();
+  avs::vecf32_t res(batch.size());
+  for (int i = 0; i < batch.size(); i++) {
+    auto d = InnerProduct(query.data(), batch[i].data(), dim);
+    res[i] = d;
+  }
+  return res;
 }
 
 } // namespace avs
